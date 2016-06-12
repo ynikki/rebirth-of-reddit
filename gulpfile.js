7a043b3fcd864913aa55f01d56425ac85f23f7cf
Nikki K <808nikz@gmail.com>
2016-06-11 14:50:59 -1000

    Got gulp to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function (){
+  gulp
+  .src('scss/styles.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'));
+});
 
 gulp.task('watch', function (){
   gulp.watch('js/app.js', function (){
